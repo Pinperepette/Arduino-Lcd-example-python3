ae8d4765bbcf2996d4cfc74479f1fbc52183d491
Pinperepette <pinperepette@gmail.com>
2020-12-15 09:46:43 +0100

    Add files via upload

--- python_code.py
+++ python_code.py
@@ -7,7 +7,8 @@ import time
 #set your port
 port = '/dev/cu.usbmodem14301'
 
-def scrivi():
+# use it to type from keyboard in real time
+def scrivi_da_tastiera():
     s = serial.Serial(port, 9600) 
     time.sleep(2) 
     s.write(('Ready...').encode('utf-8'))
@@ -18,5 +19,16 @@ def scrivi():
             break
         s.write(str)
 
+# from python_code import scrivi_come_modulo as arw
+# use it to interact with your projects
+
+def scrivi_come_modulo(text):
+    s = serial.Serial(port, 9600) 
+    time.sleep(2) 
+    str = text.encode('utf-8')
+    str = str.strip()
+    s.write(str)
+      
+
 if __name__ == "__main__":
-    scrivi()
+    scrivi_da_tastiera()
